2737ed776b3c2f3d83c9ff69914062a5746329d9
@shadowinhaze <shadowinhaze@gmail.com>
2022-06-26 20:00:31 +0200

    minor: indexing utils functions

---
+++ src/utils/index.ts
@@ -0,0 +1,6 @@
+export { getMousePos } from './get-mouse-pos';
+export { drawCircle } from './draw-circle';
+export { moveMouse } from './move-mouse';
+export { drawSquare } from './draw-square';
+export { drawRectangle } from './draw-rectangle';
+export { printScreen } from './print-screen';
